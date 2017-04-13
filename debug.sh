f64e3b8a8a4b078ceccb902ee2d5a8c822515abd
sang14 <sangya.14@gmail.com>
2017-04-13 19:42:14 +0530

    first commit

--- debug.sh
+++ debug.sh
@@ -1,2 +1,8 @@
-curl -i -X POST -H 'Content-Type: application/json' -d '{"object":"page","entry":[{"id":43674671559,"time":1460620433256,"messaging":[{"sender":{"id":1643983472284045},"recipient":{"id":1730097237216679
+curl -i -X POST -H 'Content-Type: application/json' -d '{"object":"page","entry":[{"id":43674671559,"time":1460620433256,"messaging":[{"sender":{"id":1643983472284045},"recipient":{"id":1490578364307193
 },"timestamp":1460620433123,"message":{"mid":"mid.1460620432888:f8e3412003d2d1cd93","seq":12604,"text":"Testing Chat Bot .."}}]}]}' https://antih0metheft.herokuapp.com/facebook_auth > debug.html
+
+
+
+
+
+
