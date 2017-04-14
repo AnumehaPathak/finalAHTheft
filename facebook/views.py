d88eb695cbff6fac7787d2b83837a0fda74f5a96
sang14 <sangya.14@gmail.com>
2017-04-14 10:36:56 +0530

    another commit

--- facebook/views.py
+++ facebook/views.py
@@ -18,64 +18,65 @@ VERIFY_TOKEN='AntiHomeTheft'
 PAGE_ACCESS_TOKEN='EAAKa0eCWBu4BAJjbRJv4TN3GeAd0KNKRTNHFcmJW6HZCuwDsSyatiDZBVVp8Wyz6n5ZAhEvZCohgFy4qMVUrzwxV8ZArn9v824r3uWLdwoJw112XbMhHBHfA0TvkVZBeXfgwu0ZAh2rrmkAUYgS9DK43ZBbnwKz4KhwdXJOURh93xAZDZD'
 
 def index(request):
-    output_text=find()
-    return HttpResponse(output_text,content_type='application/json')
+    post_facebook_message("1531", "Hello")
+    return HttpResponse('ok')
+
 
 def post_facebook_message(fbid,message_text):
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
-    # text,image,url_info=find(message_text)
-    # if text=='NULL':
-    #     text="No show with such name "
-    # else:
-    #     pass
+    text,image,url_info=find(message_text)
+    if text is None:
+        text="No show with such name "
+    else:
+        pass
 
-    # try:
-    #     if len(text) > 315:
-    #         text = text[3:315] + ' ...'
-    # except KeyError:
-    #     text = ''
+    try:
+        if len(text) > 315:
+            text = text[3:315] + ' ...'
+    except KeyError:
+        text = ''
 
-    # response_msg_generic={
-    #     "recipient":{
-    #         "id":fbid 
-    #       },
-    #       "message":{
-    #         "attachment":{
-    #           "type":"template",
-    #           "payload":{
-    #             "template_type":"generic",
-    #             "elements":[
-    #               {
-    #                 "title":text,
-    #                 "item_url":url_info,
-    #                 "image_url":image,
-    #                 "subtitle":"Nostalgia",
-    #                 "buttons":[
-    #                   {
-    #                     "type":"web_url",
-    #                     "url":url_info,
-    #                     "title":"View Website"
-    #                   },
-    #                   {
-    #                     "type":"postback",
-    #                     "title":"Start Chatting",
-    #                     "payload":"Another Show"
-    #                   }              
-    #                 ]
-    #               }
-    #             ]
-    #           }
-    #         }
-    #       }
+    response_msg_generic={
+        "recipient":{
+            "id":fbid 
+          },
+          "message":{
+            "attachment":{
+              "type":"template",
+              "payload":{
+                "template_type":"generic",
+                "elements":[
+                  {
+                    "title":text,
+                    "item_url":url_info,
+                    "image_url":image,
+                    "subtitle":"Nostalgia",
+                    "buttons":[
+                      {
+                        "type":"web_url",
+                        "url":url_info,
+                        "title":"View Website"
+                      },
+                      {
+                        "type":"postback",
+                        "title":"Start Chatting",
+                        "payload":"Another Show"
+                      }              
+                    ]
+                  }
+                ]
+              }
+            }
+          }
     
 
-    # }
-    response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":message_text}})
-    requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
-    #response_msg_generic=json.dumps(response_msg_generic)
+    }
+    #response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":message_text}})
+    #requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
+    response_msg_generic=json.dumps(response_msg_generic)
 
     #response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":text}})
-    #status=requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg_generic)
+    requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg_generic)
     #print status.json()
     
 
